4853f1e8c4e12b31f1b60c72244069fb5d95ee3c
Aubrey Carlon <aubreycarlon@Pauls-MacBook-Pro.local>
2023-05-30 23:30:36 -0700

    build me up

--- app/checkout/page.tsx
+++ app/checkout/page.tsx
@@ -1,17 +1,8 @@
 import PaymentForm from '@/components/PaymentForm'
-import {Elements} from '@stripe/react-stripe-js'
-import {loadStripe} from '@stripe/stripe-js'
-
-const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY as string)
 
 const CheckoutPage: React.FC = () => {
-  const options = {
-    clientSecret: '{{CLIENT_SECRET',
-  }
   return (
-    <Elements stripe={stripePromise} options={options}>
     <PaymentForm />
-    </Elements>
   )
 }
 
