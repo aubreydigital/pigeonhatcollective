d1ffbaf33780b7627f3b6e8daaee2451d6322d7a
Aubrey Carlon <aubreycarlon@Pauls-MacBook-Pro.local>
2023-06-02 00:14:37 -0700

    pesky auth route

--- app/api/auth/[...nextAuth]/route.tsx
+++ app/api/auth/[...nextAuth]/route.tsx
@@ -1,23 +1,23 @@
-import { NextAuthOptions } from 'next-auth';
-import { Session, User } from 'next-auth';
-import GoogleProvider from 'next-auth/providers/google';
+// import { NextAuthOptions } from 'next-auth';
+// import { Session, User } from 'next-auth';
+// import GoogleProvider from 'next-auth/providers/google';
 
-const providerConfig: NextAuthOptions = {
-  providers: [
-    GoogleProvider({
-      clientId: '',
-      clientSecret: '',
-    }),
-  ],
-  callbacks: {
-    async session({ session }: { session: Session }): Promise<Session> {
-      return session;
-    },
-    async signIn({ profile }: { profile: User }): Promise<any> {
-      // Perform any custom sign-in logic here
-      return true; // Return `true` to allow sign-in
-    },
-  },
-};
+// const providerConfig: NextAuthOptions = {
+//   providers: [
+//     GoogleProvider({
+//       clientId: '',
+//       clientSecret: '',
+//     }),
+//   ],
+//   callbacks: {
+//     async session({ session }: { session: Session }): Promise<Session> {
+//       return session;
+//     },
+//     async signIn({ profile }: { profile: User }): Promise<any> {
+//       // Perform any custom sign-in logic here
+//       return true; // Return `true` to allow sign-in
+//     },
+//   },
+// };
 
-export default providerConfig;
+// export default providerConfig;
